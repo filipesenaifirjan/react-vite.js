c60a11bf706da6a36390cecbb6ddbac206f7b963
filipesenaifirjan <filipemattossenai@gmail.com>
2022-02-15 15:07:04 -0300

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { Tweet } from "./components/tweet"
-
+import { Tweet } from "./components/tweet";
+import{ useState } from "react"
 
 function App() {
-  return ( <div>  <Tweet text= "Tweet 1" />
-    <Tweet />
-    <Tweet />
+  const [tweets, setTweets]  = useState<string[]>([
+    'Tweet 1',
+    'Tweet 2',
+    'Tweet 3',
+  ])
+  return (
+     <div>  
+    <Tweet text="Tweet 1"/>
+    <Tweet text="Tweet 2"/>
+    <Tweet text="Tweet 3"/>
+    <button> Adicionar tweet</button>
     </div>
   );
 }
--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,5 +1,5 @@
 type TweetProps ={
-    text: string;
+    text?: string;
 }
 
 export function Tweet(props: TweetProps) {
